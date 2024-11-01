9b25218a87d1405922b5f3411d27a78c4cd88d6d
sfK <sfk.sjm@gmail.com>
2024-11-01 09:07:00 +0900

    POST 데이터 콘텐츠 타입

--- src/main/java/com/sfk/hodolog/controller/PostController.java
+++ src/main/java/com/sfk/hodolog/controller/PostController.java
@@ -1,12 +1,19 @@
 package com.sfk.hodolog.controller;
 
+import com.sfk.hodolog.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class PostController {
-    @GetMapping("/post")
-    public String get() {
+
+    @PostMapping("/posts")
+    public String get(@RequestBody PostCreate params) {
+        log.info("params={}", params);
         return "Hello world!";
     }
 }
---
+++ src/main/java/com/sfk/hodolog/request/PostCreate.java
@@ -0,0 +1,17 @@
+package com.sfk.hodolog.request;
+
+import lombok.Setter;
+
+@Setter
+public class PostCreate {
+    public String title;
+    public String content;
+
+    @Override
+    public String toString() {
+        return "PostCreate{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
