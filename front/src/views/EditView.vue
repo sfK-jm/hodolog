3cd2e212c1bfdc32291ec7249be929e2f1733e9d
sfK <sfk.sjm@gmail.com>
2024-11-30 22:36:35 +0900

    글 수정 화면 만들기

---
+++ front/src/views/EditView.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+import axios from 'axios';
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+const post = ref({
+  id: 0,
+  title: "",
+  content: "",
+});
+
+const props = defineProps({
+  postId: {
+    type: [Number, String],
+    require: true,
+  },
+});
+
+axios.get(`/api/posts/${props.postId}`).then(response => {
+      console.log(response);
+      post.value = response.data;
+});
+
+const edit = () => {
+  axios.patch(`/api/posts/${props.postId}`, post.value).then(()=> {
+    console.log(post)
+    router.replace({name: "home"})
+  });
+}
+
+</script>
+
+<template>
+  <div>
+    <el-input v-model="post.title" />
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="post.content" type="textarea" rows="15"/>
+  </div>
+
+  <div class="mt-2">
+    <el-button type="warning" @click="edit()">글 수정완료</el-button>
+  </div>
+</template>
+
+<style>
+
+</style>
--- front/src/views/ReadView.vue
+++ front/src/views/ReadView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {onMounted, ref} from "vue";
 import axios from "axios";
+import {useRouter} from "vue-router";
 
 const props = defineProps({
   postId: {
@@ -14,6 +15,12 @@ const post = ref({
   title: "",
   content: "",
 });
+
+const router = useRouter();
+
+const moveToEdit = () => {
+  router.push({name: "edit", params: {postId: props.postId}})
+}
 
 onMounted(() => {
   console.log(props.postId)
@@ -30,6 +37,7 @@ onMounted(() => {
   <h2>{{ post.title }}</h2>
   <div>{{ post.content }}</div>
 
+  <el-button type="warning" @click="moveToEdit()">수정</el-button>
 </template>
 
 <style>
