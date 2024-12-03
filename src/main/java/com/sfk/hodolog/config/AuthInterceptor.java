337e253d005a0c07b9cf320cae59f32ee78c2fa7
sfK <sfk.sjm@gmail.com>
2024-12-03 10:41:47 +0900

    Interceptor 사용해보기

---
+++ src/main/java/com/sfk/hodolog/config/AuthInterceptor.java
@@ -0,0 +1,34 @@
+package com.sfk.hodolog.config;
+
+import com.sfk.hodolog.exception.Unauthorized;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info(">> preHandle");
+
+        String accessToken = request.getParameter("accessToken");
+        if (accessToken != null && accessToken.equals("a")) {
+            return true;
+
+        }
+        throw new Unauthorized();
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info(">> postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info(">> afterCompletion");
+    }
+}
--- src/main/java/com/sfk/hodolog/controller/PostController.java
+++ src/main/java/com/sfk/hodolog/controller/PostController.java
@@ -22,6 +22,16 @@ public class PostController {
 
     private final PostService postService;
 
+    @GetMapping("/test")
+    public String test() {
+        return "hello";
+    }
+
+    @GetMapping("/foo")
+    public String foo() {
+        return "foo";
+    }
+
     @PostMapping("/posts")
     public void post(@RequestBody @Valid PostCreate request) {
         // Case1. 저장한 데이터 Entity -> response로 응답하기
