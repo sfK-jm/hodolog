71fc0a9d02d9f06f4037958c681e4a4da26a5d9b
sfK <sfk.sjm@gmail.com>
2024-12-10 10:20:31 +0900

    JWT를 이용한 인증

--- src/main/java/com/sfk/hodolog/config/AuthResolver.java
+++ src/main/java/com/sfk/hodolog/config/AuthResolver.java
@@ -4,6 +4,10 @@ import com.sfk.hodolog.config.data.UserSession;
 import com.sfk.hodolog.domain.Session;
 import com.sfk.hodolog.exception.Unauthorized;
 import com.sfk.hodolog.repository.SessionRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +18,15 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
 @Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
     private final SessionRepository sessionRepository;
+    private static final String KEY = "HAD2DppFSBkoSyPV7H3E0IUsvVU/rQ5tVzvsfRdOI9Y=";
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -27,26 +35,49 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
-        if (servletRequest == null) {
-            log.error("servletRequest null");
+        // Session 방식
+//        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+//        if (servletRequest == null) {
+//            log.error("servletRequest null");
+//            throw new Unauthorized();
+//        }
+//
+//        Cookie[] cookies = servletRequest.getCookies();
+//
+//        if (cookies.length == 0) {
+//            log.error("쿠키가 없음");
+//            throw new Unauthorized();
+//        }
+//
+//        String accessToken = cookies[0].getValue();
+//
+//        // 데이터베이스 사용자 확인작업
+//        Session session = sessionRepository.findByAccessToken(accessToken)
+//                .orElseThrow(Unauthorized::new);
+//
+//        return new UserSession(session.getUser().getId());
+
+        //jwt 방식
+        String jws = webRequest.getHeader("Authorization");
+        if (jws == null || jws.equals("")) {
             throw new Unauthorized();
         }
 
-        Cookie[] cookies = servletRequest.getCookies();
+        byte[] decodedKey = Base64.getDecoder().decode(KEY);
 
 
-        if (cookies.length == 0) {
-            log.error("쿠키가 없음");
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(decodedKey)
+                    .build()
+                    .parseClaimsJws(jws);
+
+            String userId = claims.getPayload().getSubject();
+            return new UserSession(Long.parseLong(userId));
+
+        } catch (JwtException e) {
             throw new Unauthorized();
+            //don't trust the JWT!
         }
-
-        String accessToken = cookies[0].getValue();
-
-        // 데이터베이스 사용자 확인작업
-        Session session = sessionRepository.findByAccessToken(accessToken)
-                .orElseThrow(Unauthorized::new);
-
-        return new UserSession(session.getUser().getId());
     }
 }
--- src/main/java/com/sfk/hodolog/controller/AuthController.java
+++ src/main/java/com/sfk/hodolog/controller/AuthController.java
@@ -7,6 +7,9 @@ import com.sfk.hodolog.repository.UserRepository;
 import com.sfk.hodolog.request.Login;
 import com.sfk.hodolog.response.SessionResponse;
 import com.sfk.hodolog.service.AuthService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -16,7 +19,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.SecretKey;
+import java.security.Key;
 import java.time.Duration;
+import java.util.Base64;
 import java.util.Optional;
 
 @Slf4j
@@ -25,10 +31,13 @@ import java.util.Optional;
 public class AuthController {
 
     private final AuthService authService;
+    private final String KEY = "HAD2DppFSBkoSyPV7H3E0IUsvVU/rQ5tVzvsfRdOI9Y=";
 
     @PostMapping("/auth/login")
-    public ResponseEntity<Object> login(@RequestBody Login login) {
-        String accessToken = authService.signin(login);
+    public SessionResponse login(@RequestBody Login login) {
+        Long userId = authService.signin(login);
+/*
+        // session 방식
         ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
                 .domain("localhost") // todo 서버 환경에 따른 분리 필요
                 .path("/")
@@ -38,12 +47,18 @@ public class AuthController {
                 .sameSite("Strict")
                 .build();
 
-
-        log.info(">>>>>>>>>>>>>>>>> cookie={}", cookie.toString());
-
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .build();
+                .build();*/
 
+        // JWT
+        SecretKey key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(KEY));
+
+        String jws = Jwts.builder().
+                subject(String.valueOf(userId))
+                .signWith(key)
+                .compact();
+
+        return new SessionResponse(jws);
     }
 }
