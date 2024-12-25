f90dfd1686e36fd8912ed4c274346bfa0f920b93
sfK <sfk.sjm@gmail.com>
2024-12-25 13:53:37 +0900

    시큐리티 로그인 사용자 정보 가져오기

--- src/main/java/com/sfk/hodolog/controller/MainController.java
+++ src/main/java/com/sfk/hodolog/controller/MainController.java
@@ -1,8 +1,12 @@
 package com.sfk.hodolog.controller;
 
+import com.sfk.hodolog.config.UserPrincipal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class MainController {
 
@@ -12,7 +16,9 @@ public class MainController {
     }
 
     @GetMapping("/user")
-    public String user() {
+    public String user(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        Long userId = userPrincipal.getUserId();
+        log.info(">>>> userId:{}", userId);
         return "사용자 페이지입니다.";
     }
 
